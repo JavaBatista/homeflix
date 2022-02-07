d13e8b385f406e59190251686d0d300f1f074d41
java <java.c.batista@gmail.com>
2022-02-06 22:50:04 -0300

    Arquivo data.js  adicionado

---
+++ data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        name: "joão gomes",
+        gender: "male",
+        country: "usa",
+        city: "New york"
+    },
+
+    {
+        name: "java batista",
+        gender: "male",
+        country: "Australia",
+        city: "sidney"
+    },
+
+    {
+        name: "maria ivonete",
+        gender: "female",
+        country: "brasil",
+        city: "orocó"
+    },
+
+    {
+        name: "josemir batista",
+        gender: "male",
+        country: "brasil",
+        city: "barra"
+    }
+]
